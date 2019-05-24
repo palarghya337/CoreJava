f2464b23d8259765ae89282d0d8a2b85f94aa0fe
Arghya Pal <palarghya337@gmail.com>
2019-05-24 21:14:47 +0530

    Update SortByMultipleFields.java
    Added comments

--- src/com/corejava/practice/collections/SortByMultipleFields.java
+++ src/com/corejava/practice/collections/SortByMultipleFields.java
@@ -37,91 +37,112 @@ public class SortByMultipleFields {
 		Log.logInfo("List Size: {0}", String.valueOf(listOfEmployee.size()));
 	}
 
+	/**
+	 * Populating the {@linkplain Employee} object in the list by
+	 * shuffling the teams for project Z.
+	 * @param listOfEmployee
+	 */
 	private static void populateProjectZ(List<Employee> listOfEmployee) {
+		// shuffling names of team Z
 		listOfEmployee.add(new Employee(22100, NAME_Z, PROJECT_Z, TEAM_Z, DESIGNATION));
 		listOfEmployee.add(new Employee(22101, NAME_A, PROJECT_Z, TEAM_Z, DESIGNATION));
 		listOfEmployee.add(new Employee(22102, NAME_N, PROJECT_Z, TEAM_Z, DESIGNATION));
 		listOfEmployee.add(new Employee(22103, NAME_J, PROJECT_Z, TEAM_Z, DESIGNATION));
-		
+		// shuffling names of team A
 		listOfEmployee.add(new Employee(22104, NAME_Z, PROJECT_Z, TEAM_A, DESIGNATION));
 		listOfEmployee.add(new Employee(22105, NAME_A, PROJECT_Z, TEAM_A, DESIGNATION));
 		listOfEmployee.add(new Employee(22106, NAME_N, PROJECT_Z, TEAM_A, DESIGNATION));
 		listOfEmployee.add(new Employee(22107, NAME_J, PROJECT_Z, TEAM_A, DESIGNATION));
-		
+		// shuffling names of team Z
 		listOfEmployee.add(new Employee(22108, NAME_Z, PROJECT_Z, TEAM_N, DESIGNATION));
 		listOfEmployee.add(new Employee(22109, NAME_A, PROJECT_Z, TEAM_N, DESIGNATION));
 		listOfEmployee.add(new Employee(22110, NAME_N, PROJECT_Z, TEAM_N, DESIGNATION));
 		listOfEmployee.add(new Employee(22111, NAME_J, PROJECT_Z, TEAM_N, DESIGNATION));
-		
+		// shuffling names of team Js
 		listOfEmployee.add(new Employee(22112, NAME_Z, PROJECT_Z, TEAM_J, DESIGNATION));
 		listOfEmployee.add(new Employee(22113, NAME_A, PROJECT_Z, TEAM_J, DESIGNATION));
 		listOfEmployee.add(new Employee(22114, NAME_N, PROJECT_Z, TEAM_J, DESIGNATION));
 		listOfEmployee.add(new Employee(22115, NAME_J, PROJECT_Z, TEAM_J, DESIGNATION));
 	}
 
+	/**
+	 * Populating the {@linkplain Employee} object in the list by
+	 * shuffling the teams for project N.
+	 * @param listOfEmployee
+	 */
 	private static void populateProjectN(List<Employee> listOfEmployee) {
-		
+		//shuffling names of team Z
 		listOfEmployee.add(new Employee(22116, NAME_Z, PROJECT_N, TEAM_Z, DESIGNATION));
 		listOfEmployee.add(new Employee(22117, NAME_A, PROJECT_N, TEAM_Z, DESIGNATION));
 		listOfEmployee.add(new Employee(22118, NAME_N, PROJECT_N, TEAM_Z, DESIGNATION));
 		listOfEmployee.add(new Employee(22119, NAME_J, PROJECT_N, TEAM_Z, DESIGNATION));
-		
+		// shuffling names of team A
 		listOfEmployee.add(new Employee(22120, NAME_Z, PROJECT_N, TEAM_A, DESIGNATION));
 		listOfEmployee.add(new Employee(22121, NAME_A, PROJECT_N, TEAM_A, DESIGNATION));
 		listOfEmployee.add(new Employee(22122, NAME_N, PROJECT_N, TEAM_A, DESIGNATION));
 		listOfEmployee.add(new Employee(22123, NAME_J, PROJECT_N, TEAM_A, DESIGNATION));
-		
+		// shuffling names of team N
 		listOfEmployee.add(new Employee(22124, NAME_Z, PROJECT_N, TEAM_N, DESIGNATION));
 		listOfEmployee.add(new Employee(22125, NAME_A, PROJECT_N, TEAM_N, DESIGNATION));
 		listOfEmployee.add(new Employee(22126, NAME_N, PROJECT_N, TEAM_N, DESIGNATION));
 		listOfEmployee.add(new Employee(22127, NAME_J, PROJECT_N, TEAM_N, DESIGNATION));
-		
+		// shuffling names of team J
 		listOfEmployee.add(new Employee(22128, NAME_Z, PROJECT_N, TEAM_J, DESIGNATION));
 		listOfEmployee.add(new Employee(22129, NAME_A, PROJECT_N, TEAM_J, DESIGNATION));
 		listOfEmployee.add(new Employee(22130, NAME_N, PROJECT_N, TEAM_J, DESIGNATION));
 		listOfEmployee.add(new Employee(22131, NAME_J, PROJECT_N, TEAM_J, DESIGNATION));
 	}
 
+	/**
+	 * Populating the {@linkplain Employee} object in the list by
+	 * shuffling the teams for project J.
+	 * @param listOfEmployee
+	 */
 	private static void populateProjectJ(List<Employee> listOfEmployee) {
-		
+		// shuffling names of team Z
 		listOfEmployee.add(new Employee(22132, NAME_Z, PROJECT_J, TEAM_Z, DESIGNATION));
 		listOfEmployee.add(new Employee(22133, NAME_A, PROJECT_J, TEAM_Z, DESIGNATION));
 		listOfEmployee.add(new Employee(22134, NAME_N, PROJECT_J, TEAM_Z, DESIGNATION));
 		listOfEmployee.add(new Employee(22135, NAME_J, PROJECT_J, TEAM_Z, DESIGNATION));
-		
+		// shuffling names of team A
 		listOfEmployee.add(new Employee(22136, NAME_Z, PROJECT_J, TEAM_A, DESIGNATION));
 		listOfEmployee.add(new Employee(22137, NAME_A, PROJECT_J, TEAM_A, DESIGNATION));
 		listOfEmployee.add(new Employee(22138, NAME_N, PROJECT_J, TEAM_A, DESIGNATION));
 		listOfEmployee.add(new Employee(22139, NAME_J, PROJECT_J, TEAM_A, DESIGNATION));
-		
+		// shuffling names of team N
 		listOfEmployee.add(new Employee(22140, NAME_Z, PROJECT_J, TEAM_N, DESIGNATION));
 		listOfEmployee.add(new Employee(22141, NAME_A, PROJECT_J, TEAM_N, DESIGNATION));
 		listOfEmployee.add(new Employee(22142, NAME_N, PROJECT_J, TEAM_N, DESIGNATION));
 		listOfEmployee.add(new Employee(22143, NAME_J, PROJECT_J, TEAM_N, DESIGNATION));
-		
+		// shuffling names of team J
 		listOfEmployee.add(new Employee(22144, NAME_Z, PROJECT_J, TEAM_J, DESIGNATION));
 		listOfEmployee.add(new Employee(22145, NAME_A, PROJECT_J, TEAM_J, DESIGNATION));
 		listOfEmployee.add(new Employee(22146, NAME_N, PROJECT_J, TEAM_J, DESIGNATION));
 		listOfEmployee.add(new Employee(22147, NAME_J, PROJECT_J, TEAM_J, DESIGNATION));
 	}
 
+	/**
+	 * Populating the {@linkplain Employee} object in the list by
+	 * shuffling the teams for project A.
+	 * @param listOfEmployee
+	 */
 	private static void populateProjectA(List<Employee> listOfEmployee) {
-		
+		// shuffling names of team Z
 		listOfEmployee.add(new Employee(22148, NAME_Z, PROJECT_A, TEAM_Z, DESIGNATION));
 		listOfEmployee.add(new Employee(22149, NAME_A, PROJECT_A, TEAM_Z, DESIGNATION));
 		listOfEmployee.add(new Employee(22150, NAME_N, PROJECT_A, TEAM_Z, DESIGNATION));
 		listOfEmployee.add(new Employee(22151, NAME_J, PROJECT_A, TEAM_Z, DESIGNATION));
-		
+		// shuffling names of team A
 		listOfEmployee.add(new Employee(22152, NAME_Z, PROJECT_A, TEAM_A, DESIGNATION));
 		listOfEmployee.add(new Employee(22153, NAME_A, PROJECT_A, TEAM_A, DESIGNATION));
 		listOfEmployee.add(new Employee(22154, NAME_N, PROJECT_A, TEAM_A, DESIGNATION));
 		listOfEmployee.add(new Employee(22155, NAME_J, PROJECT_A, TEAM_A, DESIGNATION));
-		
+		// shuffling names of team N
 		listOfEmployee.add(new Employee(22156, NAME_Z, PROJECT_A, TEAM_N, DESIGNATION));
 		listOfEmployee.add(new Employee(22157, NAME_A, PROJECT_A, TEAM_N, DESIGNATION));
 		listOfEmployee.add(new Employee(22158, NAME_N, PROJECT_A, TEAM_N, DESIGNATION));
 		listOfEmployee.add(new Employee(22159, NAME_J, PROJECT_A, TEAM_N, DESIGNATION));
-		
+		// shuffling names of team J
 		listOfEmployee.add(new Employee(22160, NAME_Z, PROJECT_A, TEAM_J, DESIGNATION));
 		listOfEmployee.add(new Employee(22161, NAME_A, PROJECT_A, TEAM_J, DESIGNATION));
 		listOfEmployee.add(new Employee(22162, NAME_N, PROJECT_A, TEAM_J, DESIGNATION));
