32d01a850e09e6707e0754308f85188d9982ce68
Arghya Pal <palarghya337@gmail.com>
2019-05-29 21:52:47 +0530

    Update SingleThreadExecutorExample.java
    Changed the name

--- src/com/corejava/practice/multithreading/SingleThreadExecutorExample.java
+++ src/com/corejava/practice/multithreading/SingleThreadExecutorExample.java
@@ -3,7 +3,7 @@ package com.corejava.practice.multithreading;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class SingleThreadPoolExample {
+public class SingleThreadExecutorExample {
 
 	public static void main(String[] args) {
 		/* Creates an Executor that uses a single worker thread
