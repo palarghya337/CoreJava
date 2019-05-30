2bfb25a7bd423ffe58a0a9a83d80c5fdd2df9bb6
Arghya Pal <arghya.pal@factset.com>
2019-05-30 08:50:49 +0530

    Fixed Printing value more than the given range.
    When thread number is more than range to print, it was printing more
    than the range. This was happening because we were not checking the
    condition again after taking the lock. When current thread was waiting
    to acquire the lock, different thread might have updated the value for
    the number and when current thread acquired the lock, as it was not
    checking the condition again, it was printing the value more than the
    range given.

--- src/com/corejava/practice/multithreading/PrintNumberSequentially.java
+++ src/com/corejava/practice/multithreading/PrintNumberSequentially.java
@@ -1,5 +1,7 @@
 package com.corejava.practice.multithreading;
 
+import java.util.stream.IntStream;
+
 import com.corejava.practice.utils.Log;
 
 /**
@@ -12,59 +14,53 @@ public class PrintNumberSequentially {
 
 	public static void main(String[] args) {
 
-		PrintNumber obj = new PrintNumber(8);
-		Thread t1 = new Thread(obj);
-		Thread t2 = new Thread(obj);
-		Thread t3 = new Thread(obj);
-		Thread t4 = new Thread(obj);
-		Thread t5 = new Thread(obj);
-		t1.start();
-		t2.start();
-		t3.start();
-		t4.start();
-		t5.start();
-
+    PrintNumber obj = new PrintNumber(10);
+    IntStream.range(0, 3).forEach(index -> new Thread(obj).start());
 	}
 }
 class PrintNumber implements Runnable {
 	
 	private int number;
-	private int iterationTime;
+	private int range;
 	
-	public PrintNumber(int iterationTime) {
-		this.iterationTime = iterationTime;
+	public PrintNumber(int range) {
+		this.range = range;
 	}
 	
 	@Override
 	public void run() {
 		
-		while (number < iterationTime) {
+		while (number < range) {
 			synchronized (this) {
-				
-				Log.logInfo("{0}, Number-{1}", Thread.currentThread().getName(), number++);
-				/* This notify is to notify all the other thread
-				 * if any thread is waiting to acquire the lock
-				 * to print the number.
-				 * */
-				this.notifyAll();
-				try {
-					/* Waiting the current thread so that other
-					 * thread can also get a chance to print the
-					 * number.
-					 **/
-					this.wait();
-				} catch (InterruptedException e) {
-					Log.logInfo("Exception: {0}", e.getMessage());
-					Thread.currentThread().interrupt();
-				}
-				/* This notify is use to notify all the waiting thread
-				 * to finish the task after the condition fulfills(i.e.-
-				 * number became greater or equals the iterationTime)
-				 * If we did not notify all the thread then deadlock
-				 * would happen because waiting thread will keep on
-				 * waiting.
-				 **/
-				this.notifyAll();
+        /* Checking the condition again because different thread might have
+         * updated the number while current thread is waiting to acquire the lock.
+         **/
+        if (number < range) {
+          Log.logInfo("{0}, Number-{1}", Thread.currentThread().getName(), number++);
+          /* This notify is to notify all the other thread
+           * if any thread is waiting to acquire the lock
+           * to print the number.
+           * */
+          this.notifyAll();
+          try {
+            /* Waiting the current thread so that other
+             * thread can also get a chance to print the
+             * number.
+             **/
+            this.wait();
+          } catch (InterruptedException e) {
+            Log.logInfo("Exception: {0}", e.getMessage());
+            Thread.currentThread().interrupt();
+          }
+          /* This notify is use to notify all the waiting thread
+           * to finish the task after the condition fulfills(i.e.-
+           * number became greater or equals the iterationTime)
+           * If we did not notify all the thread then deadlock
+           * would happen because waiting thread will keep on
+           * waiting.
+           **/
+          this.notifyAll();
+        }
 			}
 		}
 	}
