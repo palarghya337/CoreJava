3470c7d719fe949a1d2d4f209e881e5cbed5bdf3
Arghya Pal <arghya.pal@factset.com>
2019-05-30 12:10:54 +0530

    Modified few cases.

--- src/com/corejava/practice/multithreading/ProducerConsumerProblem.java
+++ src/com/corejava/practice/multithreading/ProducerConsumerProblem.java
@@ -2,7 +2,7 @@ package com.corejava.practice.multithreading;
 
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.Random;
 import java.util.stream.IntStream;
 
 import com.corejava.practice.utils.Log;
@@ -21,23 +21,34 @@ public class ProducerConsumerProblem {
 
   public static void main(String[] args) {
 
+    // Custom queue which will manage producer-consumer problem.
     MyBlockingQueue<Integer> queue = new MyBlockingQueue<>(10);
-    new LinkedBlockingQueue<>();
-    // Thread-1 is the producer thread.
-    Runnable producer = () -> IntStream.range(0, 4).forEach(index -> {
-      queue.put(index);
-      Log.logInfo("Thread: {0}, Produced: {1}", Thread.currentThread().getName(), index);
+    Random randomInteger = new Random();
+
+    Runnable producerRunnable = () -> IntStream.range(0, 4).forEach(index -> {
+      int ranIn = randomInteger.nextInt(20);
+      queue.put(ranIn);
+      Log.logInfo("Thread: {0}, Produced: {1}", Thread.currentThread().getName(), ranIn);
     });
-    Runnable consumer = () -> {
+
+    Runnable producerRunnable1 = () -> IntStream.range(0, 4).forEach(index -> {
+      int ranIn = randomInteger.nextInt(20);
+      queue.put(ranIn);
+      Log.logInfo("Thread: {0}, Produced: {1}", Thread.currentThread().getName(), ranIn);
+    });
+
+    Runnable consumerRunnable = () -> {
       while (true) {
         Log.logInfo("Thread: {0}, Consumed: {1}", Thread.currentThread().getName(), queue.take());
       }
     };
 
-    Thread proThread = new Thread(producer);
-    Thread conThread = new Thread(consumer);
-    proThread.start();
-    conThread.start();
+    Thread producerThread = new Thread(producerRunnable);
+    Thread producerThread1 = new Thread(producerRunnable1);
+    Thread consumerThread = new Thread(consumerRunnable);
+    producerThread.start();
+    producerThread1.start();
+    consumerThread.start();
   }
 }
 
@@ -53,6 +64,14 @@ class MyBlockingQueue<E> {
     queue = new LinkedList<>();
   }
 
+  /**
+   * This method add the data to the queue if space is available
+   * in the queue. If space is not available then it will wait
+   * to remove some data from the queue and space to become available.
+   * 
+   * @param value
+   * @return boolean
+   */
   public boolean put(E value) {
     synchronized (full) {
       while (queue.size() == maxSize) {
@@ -71,6 +90,13 @@ class MyBlockingQueue<E> {
     }
   }
 
+  /**
+   * This method removes the data from the queue if queue is not
+   * empty. If queue is empty then it will wait until some data
+   * is available in the queue to consume.
+   * 
+   * @return E
+   */
   public E take() {
 
     synchronized (empty) {
@@ -84,9 +110,18 @@ class MyBlockingQueue<E> {
       }
     }
     synchronized (full) {
-      E value = queue.remove();
+      E value = queue.poll();
       full.notifyAll();
       return value;
     }
+  }
+
+  /**
+   * If size of the queue is zero then this method will return
+   * true otherwise false.
+   * @return boolean
+   */
+  public boolean isEmpty() {
+    return queue.isEmpty();
   }
 }
