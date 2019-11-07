50557eb5c117aaddca94be5ca133d63417be3ae7
Arghya Pal <palarghya337@gmail.com>
2019-11-07 10:26:38 +0530

    Update OverridingExample.java

--- src/com/corejava/practice/polymorphism/OverridingExample.java
+++ src/com/corejava/practice/polymorphism/OverridingExample.java
@@ -6,51 +6,59 @@ import com.corejava.practice.utils.Log;
 
 public class OverridingExample extends Parent {
 
-  public static void main(String[] args) {
-    OverridingExample overridingExample = new OverridingExample();
-    overridingExample.method1();
-    try {
-      overridingExample.method2();
-    } catch (CheckedException e1) {
-      Log.logInfo("Handled checked exception. {0}", e1.getMessage());
-    }
-    overridingExample.method5();
-    Parent parent = new OverridingExample();
-    try {
-      parent.method2();
-    } catch (Exception e) {
-      Log.logInfo("Exception: {0}", e.getMessage());
-    }
-  }
+	public static void main(String[] args) {
+		OverridingExample overridingExample = new OverridingExample();
+		overridingExample.method1();
+		try {
+			overridingExample.method2();
+		} catch (CheckedException e1) {
+			Log.logInfo("Handled checked exception. {0}", e1.getMessage());
+		}
+		overridingExample.method5();
+		Parent parent = new OverridingExample();
+		try {
+			parent.method2();
+		} catch (Exception e) {
+			Log.logInfo("Exception: {0}", e.getMessage());
+		}
+	}
 
-  @Override
-  protected void method2() throws CheckedException {
-    Log.logInfo("Overridden method is throwing child checked exception which is allowed.");
-    throw new CheckedException("Throwing CheckedException from " + getCurrentMethod());
-  }
+	/*
+	 * This is not allowed
+	 * @Override protected void method1() throws CheckedException { // TODO
+	 * Auto-generated method stub super.method1(); }
+	 */
+	@Override
+	protected void method1() throws UncheckedException {
+		Log.logInfo("Overridden method is throwing unchecked exception which is allowed.");
+	}
+	@Override
+	protected void method2() throws CheckedException {
+		Log.logInfo("Overridden method is throwing child checked exception which is allowed.");
+		throw new CheckedException("Throwing CheckedException from " + getCurrentMethod());
+	}
 
-  @Override
-  protected void method4() {
-    Log.logInfo("Overridden method is not throwing any exception which is allowed.");
-  }
+	/*
+	 * This is not allowed
+	 * @Override protected void method3() throws CheckedException { }
+	 */
+	@Override
+	protected void method4() {
+		Log.logInfo("Overridden method is not throwing any exception which is allowed.");
+	}
 
-  /**
-   * This will throw the Compile time exception as throwing
-   * parent exception is not allowed in the overloaded exception.
-   * 
-   */
-  /*
-  @Override
-  protected void method3() throws Exception {
-    System.out.println(getCurrentMethod());
-  }*/
+	/**
+	 * This will throw the Compile time exception as throwing
+	 * parent exception is not allowed in the overloaded exception.
+	 * 
+	 */
 
-  @Override
-  protected String method5() {
-    Log.logInfo(
-        "Overriding is possible by changeing the returntype.\n If child class is returning an object of a sub class type.");
-    return "Returning object of String class";
-  }
+	@Override
+	protected String method5() {
+		Log.logInfo(
+				"Overriding is possible by changeing the returntype.\n If child class is returning an object of a sub class type.");
+		return "Returning object of String class";
+	}
 }
 
 class Parent {
